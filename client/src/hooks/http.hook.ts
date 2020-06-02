5964bad5f4a70b8cbfda6a33302967297836900d
Striukov Konstantin <sharkeron@gmail.com>
2020-06-02 21:33:27 +0300

    Added http hook. Added partial auth page on client. Added proxy to package.json to client.

---
+++ client/src/hooks/http.hook.ts
@@ -0,0 +1,61 @@
+import {useCallback, useState} from 'react';
+
+declare namespace Request {
+   export type RequestMethodsType = 'GET' | 'POST' | 'PUT' | 'DELETE';
+
+   export interface Headers {
+      [header: string]: any;
+   }
+}
+
+export const useHttp = () => {
+   const [loading, setLoading] = useState(false);
+   const [error, setError] = useState(null);
+
+   const request = useCallback(async <Body>(
+      url: string,
+      method: Request.RequestMethodsType = 'GET',
+      body?: {},
+      headers: Partial<Request.Headers> = {},
+   ) => {
+      setLoading(true);
+
+      if (body) {
+         body = JSON.stringify(body) as string;
+         headers['Content-Type'] = 'application/json';
+      }
+
+      try {
+         const response = await fetch(
+            url,
+            {
+               method,
+               body: body as string | undefined,
+               headers,
+            },
+         );
+         const data = await response.json();
+
+         if (!response.ok) {
+            throw new Error(data.message || 'Something went wrong');
+         }
+
+         setLoading(false);
+
+         return data;
+      } catch (e) {
+         setLoading(false);
+         setError(e.message);
+         throw e;
+      }
+   }, []);
+
+   const clearError = () => setError(null);
+
+   return {
+      loading,
+      request,
+      error,
+      clearError,
+   };
+};
--- client/src/pages/AuthPage.tsx
+++ client/src/pages/AuthPage.tsx
@@ -1,9 +1,101 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useHttp} from '../hooks/http.hook';
+
+interface authPageInterface {
+    email: string;
+    password: string;
+    remember: boolean;
+}
 
 export const AuthPage = () => {
+    const {loading, request, error} = useHttp();
+    const [form, setForm] = useState<authPageInterface>({
+        email: '',
+        password: '',
+        remember: false,
+    });
+
+    const changeHandler = (event: React.FormEvent<HTMLInputElement>) => {
+        setForm({...form, [event.currentTarget.name]: event.currentTarget.value});
+    };
+
+    const registerHandler = async () => {
+        try {
+            const data = await request('/api/auth/sign_up', 'POST', {...form});
+
+            console.log(data);
+        } catch (e) {
+
+        }
+    };
+
     return (
-            <div>
-                <h1>Auth Page</h1>
+            <div className="row">
+                <div className="col s6 offset-s3">
+                    <h1>Links shortener</h1>
+
+                    <div className="card blue-grey">
+                        <div className="card-content white-text">
+                            <div className="card-title">
+                                Login page
+                            </div>
+
+                            <div className="input-field">
+                                <input type="text"
+                                       placeholder="Email"
+                                       className="yellow-input"
+                                       id="email"
+                                       name="email"
+                                       onChange={changeHandler}/>
+
+                                <label htmlFor="email"
+                                       className="active">
+                                    Email
+                                </label>
+                            </div>
+
+                            <div className="input-field">
+                                <input type="password"
+                                       placeholder="Password"
+                                       className="yellow-input"
+                                       id="password"
+                                       name="password"
+                                       onChange={changeHandler}/>
+
+                                <label htmlFor="email"
+                                       className="active">
+                                    Password
+                                </label>
+                            </div>
+
+                            <label>
+                                <input type="checkbox"
+                                       className="filled-in"
+                                       name="remember"
+                                       onChange={changeHandler}/>
+
+                                <span className="white-text">
+                                    Remember me
+                                </span>
+                            </label>
+                        </div>
+                    </div>
+
+                    <div className="card-action">
+                        <button className="btn yellow darken-4 mr-1"
+                                type="button"
+                                disabled={loading}>
+                            Sign in
+                        </button>
+
+                        <button className="btn grey lighten-1 black-text"
+                                type="button"
+                                onClick={registerHandler}
+                                disabled={loading}>
+                            Sign up
+                        </button>
+                    </div>
+                </div>
             </div>
     );
 };
