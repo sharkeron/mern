1f1d6ac0b714150c4b484d8cb76e7ca5df2b51f0
Striukov Konstantin <sharkeron@gmail.com>
2020-06-04 17:37:44 +0300

    Fixed environment variables

--- config/prod.ts
+++ config/prod.ts
@@ -1,8 +1,8 @@
 import {EnvConfigInterface} from './config.interface';
 
-export const _: EnvConfigInterface = {
+module.exports = {
    port: <string>process.env.PORT,
    mongoUri: <string>process.env.MONGO_URI,
    jwtSecret: <string>process.env.JWT_SECRET,
    baseUrl: <string>process.env.BASE_URL,
-};
+} as EnvConfigInterface;
